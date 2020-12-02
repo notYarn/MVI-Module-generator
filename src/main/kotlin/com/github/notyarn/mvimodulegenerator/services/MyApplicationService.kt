be97e9c242f13dc5cf1b2c1a656ea8b13a3e0c6e
GitHub Action <action@github.com>
2020-12-02 16:51:21 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/notyarn/mvimodulegenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.notyarn.mvimodulegenerator.services
+
+import com.github.notyarn.mvimodulegenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
